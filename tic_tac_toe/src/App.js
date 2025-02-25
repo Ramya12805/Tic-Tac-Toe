a28a837d37d4b969e22d12102787a6b01dd50394
Polisetty Ramya Sri <polisettyramyasri12@gmail.com>
2025-02-25 22:30:06 +0530

    Added title

--- tic_tac_toe/src/App.js
+++ tic_tac_toe/src/App.js
@@ -88,14 +88,18 @@ export default function Game() {
 
   return (
     <div className="game">
-      <div className="game-board">
-        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
-      </div>
-      <div className="game-info">
-        <ol>{moves}</ol>
+      <h1 className="game-title">Tic Tac Toe</h1>
+      <div className="game-container">
+        <div className="game-board">
+          <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+        </div>
+        <div className="game-info">
+          <ol>{moves}</ol>
+        </div>
       </div>
     </div>
   );
+  
 }
 
 function calculateWinner(squares) {
